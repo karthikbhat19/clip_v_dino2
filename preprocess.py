86527412f8dc001957f3080695abdfa6a54c265d
Karthik <kabhat@ucsc.edu>
2024-01-31 16:15:43 -0800

    Set to give max_length caption

--- preprocess.py
+++ preprocess.py
@@ -8,4 +8,8 @@ for index, row in df.iterrows():
         processed_dict[row['image']] = []
     processed_dict[row['image']].append(row['caption'])
 
+for k, v in processed_dict.items():
+    lengths = [len(s) for s in v]
+    processed_dict[k] = v[lengths.index(max(lengths))]
+
 print(processed_dict['1001773457_577c3a7d70.jpg'])
